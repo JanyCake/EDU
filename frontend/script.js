4ddcb5f07fdd568de3ac1e7e1f5a48fa7ffc1275
JanyCake <102861777+JanyCake@users.noreply.github.com>
2025-06-01 20:38:13 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ async function askQuestion() {
     responseDiv.textContent = 'Загрузка...';
 
     try {
-        const res = await fetch('http://localhost:5000/api/question', {
+        const res = await fetch('https://edu-9pdm.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ question })
